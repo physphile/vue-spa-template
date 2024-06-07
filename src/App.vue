37ace412c0c2aa08f7a5ba8df1959659829994e4
Artem Netsvetaev <physphile@vk.com>
2024-06-08 02:05:19 +0300

    Добавил router и pinia, обновил зависимости

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
 
 <template>
-	<h1>Vue SFC template</h1>
+	<RouterView />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from "vue";
 import "./styles/reset.css";
 import "./styles/style.css";
+import { createPinia } from "pinia";
+import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
+import { routes } from "./routes/main";
 
-createApp(App).mount("#app");
+createApp(App)
+	.use(createPinia())
+	.use(
+		createRouter({
+			history: createWebHistory(),
+			routes,
+		})
+	)
+	.mount("#app");
